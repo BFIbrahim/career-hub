9f73989e5a051bdb43cd28e9e32fba988ca70cf4
BFIbrahim <ibrahim74543@gmail.com>
2023-04-10 08:23:42 -0700

    Home Banner added

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -1,23 +1,25 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link, NavLink } from 'react-router-dom';
 import './Header.css'
+import { BeakerIcon, Bars3BottomRightIcon, XMarkIcon } from '@heroicons/react/24/solid'
 
 const Header = () => {
+    const [isMenuOpen, setIsMenuOpen] = useState(false)
     return (
-        <div className='flex items-center justify-between w-5/6 mx-auto mb-3'>
-            <div>
+        <div className='bg-gray-100 px-4 py-5 mx-auto sm:max-w-xl md:max-w-full lg:max-w-screen-xl md:px-24 lg:px-8'>
+            <div className='relative flex items-center justify-between'>
+                {/* Logo Section */}
                 <Link to='/' className='inline-flex items-center'>
-                    <span className='ml-2 text-xl font-bold tracking-wide'>
+                    <span className='ml-2 text-xl font-bold tracking-wide text-gray-800'>
                         Big Future Career
                     </span>
                 </Link>
-            </div>
 
-            <div className='flex justify-between'>
+                {/* Nav Items Section */}
                 <ul className='items-center hidden space-x-8 lg:flex'>
                     <li>
                         <NavLink
-                            to='/Statistics'
+                            to='/statistics'
                             className={({ isActive }) => (isActive ? 'active' : 'default')}
                         >
                             Statistics
@@ -30,23 +32,88 @@ const Header = () => {
                         >
                             Applied Jobs
                         </NavLink>
-
                     </li>
-
                     <li>
                         <NavLink
-                            to='/Blog'
+                            to='/blog'
                             className={({ isActive }) => (isActive ? 'active' : 'default')}
                         >
                             Blog
                         </NavLink>
-
                     </li>
                 </ul>
-            </div>
 
-            <div>
-                <button className='apply-btn'>Star Applying</button>
+                <div className='hidden md:block lg:block'>
+                    <button className='apply-btn'>Star Applying</button>
+                </div>
+
+
+        {/* Small device navigation */}
+                <div className='lg:hidden'>
+                    <button
+                        aria-label='Open Menu'
+                        title='Open Menu'
+                        onClick={() => setIsMenuOpen(true)}
+                    >
+                        <Bars3BottomRightIcon className='w-5 text-gray-600' />
+                    </button>
+                    {isMenuOpen && (
+                        <div className='absolute top-0 left-0 w-full z-10'>
+                            <div className='p-5 bg-white border rounded shadow-sm'>
+                                {/* Logo & Button section */}
+                                <div className='flex items-center justify-between mb-4'>
+                                    <div>
+                                        <Link to='/' className='inline-flex items-center'>
+                                            <span className='ml-2 text-xl font-bold tracking-wide text-gray-800 uppercase'>
+                                                Big Future Career
+                                            </span>
+                                        </Link>
+                                    </div>
+
+                                    <div>
+                                        <button
+                                            aria-label='Close Menu'
+                                            title='Close Menu'
+                                            onClick={() => setIsMenuOpen(false)}
+                                        >
+                                            <XMarkIcon className='w-5 text-gray-600' />
+                                        </button>
+                                    </div>
+                                </div>
+
+                                <nav>
+                                    <ul className='space-y-4'>
+                                        <li>
+                                            <Link to='/Statistics' className='default'>
+                                            Statistics
+                                            </Link>
+                                        </li>
+                                        <li>
+                                            <Link
+                                                to='/Applied Jobs'
+                                                className='font-medium tracking-wide text-gray-700 transition-colors duration-200 hover:text-blue-400'
+                                            >
+                                                Applied Jobs
+                                            </Link>
+                                        </li>
+                                        <li>
+                                            <Link
+                                                to='/blog'
+                                                className='font-medium tracking-wide text-gray-700 transition-colors duration-200 hover:text-blue-400'
+                                            >
+                                                Blog
+                                            </Link>
+                                        </li>
+                                    </ul>
+
+                                    <div className=''>
+                                        <button className='apply-btn mt-2'>Star Applying</button>
+                                    </div>
+                                </nav>
+                            </div>
+                        </div>
+                    )}
+                </div>
             </div>
         </div>
     );
--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -1,9 +1,20 @@
 import React from 'react';
+import './Home.css'
 
 const Home = () => {
     return (
-        <div>
-            <h1>This Is Home</h1>
+        <div className='md:flex items-center md:bg-gray-100 md:text-start'>
+            <div >
+                <h1 className='text-5xl font-bold leading-tight md:ml-6'>One Step <br /> Closer To Your <br /><span className='header-text'>Dream Job</span></h1>
+
+                <p className='text-1xl mt-3 md:ml-6'>Explore thousands of job opportunities with all the information you need. Its your future. Come find it. Manage all your job application from start to finish.</p>
+
+                <button className='banner-button mt-5 md:ml-6'>Get Started</button>
+            </div>
+
+            <div>
+                <img src="../../../public/Images/man.png" alt="" />
+            </div>
         </div>
     );
 };
