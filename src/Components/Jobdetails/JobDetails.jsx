79a29ac152727743c7147d5b7758ea33f230a255
BFIbrahim <ibrahim74543@gmail.com>
2023-04-12 03:37:37 -0700

    Job details button added

--- src/Components/Jobdetails/JobDetails.jsx
+++ src/Components/Jobdetails/JobDetails.jsx
@@ -1,21 +1,59 @@
 import React from 'react';
 import './JobDetails.css'
 import { useLoaderData } from 'react-router-dom';
+import { CalendarDaysIcon, CurrencyDollarIcon, MapPinIcon, PhoneIcon } from '@heroicons/react/24/solid'
 
 const JobDetails = () => {
 
-    const singleJobDetail =  useLoaderData()
-    
+    const singleJobDetail = useLoaderData()
+
+    const { jobDescription, jobResponsibility, educationalRequirements, experiences, salary, jobTitle, contactInformation, location } = singleJobDetail
+
     return (
         <div>
             <div className='bg-gray-100 p-14 details-page'>
                 <h1 className='font-bold text-2xl '>Job Details</h1>
-            </div> 
-
-
-            <div>
-                
             </div>
+
+
+            <div className='w-4/5 mx-auto md:flex gap-14  mt-14 items-center'>
+                <div className='text-start leading-8 md:w-3/6'>
+                    <p><strong>job Description:</strong> {jobDescription}</p>
+                    <p><strong>job Responsibility: </strong> {jobResponsibility}</p>
+                    <p><strong>Educational Requirements:</strong></p>
+                    <p>{educationalRequirements}</p>
+                    <p><strong>Experiences</strong></p>
+                    <p>{experiences}</p>
+                </div>
+
+                <div className='p-10 bg-gray-100 rounded-sm mt-5'>
+                    <h3 className='border-b-2 border-purple-300'>Job Details</h3><br />
+                    <p className='flex gap-5'>
+                    <span><CurrencyDollarIcon className='w-6 h-6 text-purple-400'></CurrencyDollarIcon></span>
+                    <span>Salary: {salary}</span>
+                    </p><br />
+
+                    <p className='flex gap-5 border-b-2 border-purple-300 pb-4'>
+                        <CalendarDaysIcon className='w-6 h-6 text-purple-400'></CalendarDaysIcon>
+                        <span>Job Title: {jobTitle}</span>
+                    </p><br />
+                    
+
+                    <p className='flex gap-5'>
+                        <PhoneIcon  className='w-6 h-6 text-purple-400'></PhoneIcon>
+                        <span>Contact: {contactInformation}</span>
+                    </p><br />
+
+                    <p className='flex gap-5'>
+                        <MapPinIcon  className='w-6 h-6 text-purple-400'></MapPinIcon>
+                        <span>Location: {location}</span>
+                    </p>
+
+                    <button className='apply-btn mt-10 w-5/5'>Apply Now</button>
+                </div>
+            </div>
+
+
         </div>
     );
 };
--- src/Components/SingleFeatureJob/SingleFeatureJob.jsx
+++ src/Components/SingleFeatureJob/SingleFeatureJob.jsx
@@ -1,14 +1,15 @@
 import React from 'react';
 import './SingleFeatureJob.css'
 import { Link } from 'react-router-dom';
+import { CurrencyDollarIcon, MapPinIcon } from '@heroicons/react/24/solid'
 
 
-const SingleFeatureJob = ({feturedJob}) => {
-// console.log(feturedJob);
+const SingleFeatureJob = ({ feturedJob }) => {
+    // console.log(feturedJob);
 
-const detailsClick = () => {
+    const detailsClick = () => {
 
-}
+    }
     return (
         <div>
             <div className='text-start mb-14 text-gray-600'>
@@ -17,8 +18,14 @@ const detailsClick = () => {
                 <h5>{feturedJob.companyName}</h5>
                 <button className='feature-btn mt-3 mb-3'>{feturedJob.remoteOrOnsite}</button>
                 <button className='feature-btn ml-2 mt-3 mb-3'>{feturedJob.fulltimeOrParttime}</button><br />
-                <span>{feturedJob.location}</span>
-                <span className='ml-5'>Salary: {feturedJob.salary}</span><br />
+                <p className='flex gap-2'>
+                    <MapPinIcon className='w-6 h-6  text-gray-500'></MapPinIcon>
+                    <span>{feturedJob.location}</span>
+                </p>
+                <p className='flex gap-2'>
+                    <CurrencyDollarIcon className='w-6 h-6 text-gray-500'></CurrencyDollarIcon>
+                    <span>Salary: {feturedJob.salary}</span><br />
+                </p>
 
                 <Link to={`/details/${feturedJob.id}`}><button className='deyails-btn'>View Details</button></Link>
             </div>
